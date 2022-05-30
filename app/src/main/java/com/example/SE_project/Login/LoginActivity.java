5d6033958f8d48a4e6fb47b716c5c77723d2a51d
BetterTomorrowThanToday <wsx0239@gmail.com>
2022-05-31 01:39:23 +0900

    MOD: UI & features update

--- app/src/main/java/com/example/SE_project/Login/LoginActivity.java
+++ app/src/main/java/com/example/SE_project/Login/LoginActivity.java
@@ -32,18 +32,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-       // AppEventsLogger.activateApp(getApplication());
-        //login_K = findViewById(R.id.loginK); //카카오로그인
-        //logout_K = findViewById(R.id.logoutK);
+
         Button loginE = findViewById(R.id.loginB);
-        //ImageButton loginK = findViewById(R.id.btnKako);
-//        loginK.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), LoginActivity1.class);
-//                startActivity(intent);
-//            }
-//        });
+
         Button FindPW = (Button) findViewById(R.id.findPW);
         FindPW.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,95 +47,18 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance(); //파이어베이스어스객체의 공유 인스턴스를 가져옴
         findViewById(R.id.loginB).setOnClickListener(onClickListener);
         findViewById(R.id.sb).setOnClickListener(onClickListener);
-//        mCallbackManager = CallbackManager.Factory.create();
-//        LoginButton loginButton = findViewById(R.id.loginF);
-//        loginButton.setPermissions("email", "public_profile");
-//        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
-//            @Override
-//            public void onSuccess(LoginResult loginResult) {
-//                handleFacebookAccessToken(loginResult.getAccessToken());
-//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                startActivity(intent);
-//            }
-//
-//            @Override
-//            public void onCancel() {
-//
-//            }
-//
-//            @Override
-//            public void onError(FacebookException error) {
-//
-//            }
-//        });
-
-//        authStateListener = new FirebaseAuth.AuthStateListener() {
-//            //파이어베이스는 싱글톤패턴을 이용하기 때문에 로그아웃을 하지않으면 계속 인증상태임
-//            //인증되어있을때는 로그인화면에서 그냥 바로 메인으로 이동하려면 OnConnectionFailedListener 사용
-//            @Override
-//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-//                FirebaseUser user = firebaseAuth.getCurrentUser();
-//                if (user != null) {
-//                    //현재 유저가 로그인 되어있는걸 확인한 후 그냥 바로 메인으로 연결해줌
-//                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                    startActivity(intent);
-//                    finish();
-//                } else {
-//                    //로그아웃 상태
-//                }
-//            }
-//        };
-
     }
 
     @Override
     public void onStart() {
         super.onStart();
-
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        updateUI(currentUser);
-//        if (currentUser != null) {
-//            currentUser.reload();
-//        }
-        super.onStart();
-
     }
+
     @Override
     protected void onStop(){
         super.onStop();
         mAuth.removeAuthStateListener(authStateListener);
     }
-
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        // 페이스북 콜백 등록
-//        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-//    }
-
-//    private void handleFacebookAccessToken(AccessToken token) {
-//        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-//        mAuth.signInWithCredential(credential)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            // Sign in success, update UI with the signed-in user's information
-//                            Log . d (TAG, "FFAACCEEBBOOKK");
-//                            FirebaseUser user = mAuth.getCurrentUser();
-//                            updateUI(user);
-//
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//                            Log . d (TAG, "FFAAIILL");
-//                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-//                                    Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
-//
-//                        }
-//                    }
-//                });
-//    }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
